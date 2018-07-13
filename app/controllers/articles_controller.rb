d5ed46fd4387ec0f1671e78403d2ab146221d320
castleArg <markchurcher@gmail.com>
2018-07-13 16:44:49 +1200

    validation on new posts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
     def new
+        @article = Article.new()
     end
 
     def index
@@ -12,8 +13,11 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        @article.save
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
 
     private
