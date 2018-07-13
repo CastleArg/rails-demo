c139f5b29b61c0003680c11fedf803f6e7557a74
castleArg <markchurcher@gmail.com>
2018-07-13 17:52:53 +1200

    add edit and delete features

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'articles/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
-  root 'welcome#index'
+  root 'articles#index'
 end
